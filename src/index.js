b9ce917722638fe844158fee4709d6b1810efc6c
capricornus44 <antonzavalniuk@gmail.com>
2021-03-03 01:04:19 +0100

    Update index.js
    add the ability to change the theme and create menu markup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
 import './styles.css';
+
+//==================== MENU MARKUP CREATING ====================
+import menuElemsTmpl from './template/menu-elems.hbs';
+import menuElems from './menu.json';
+
+const menuContainer = document.querySelector('.js-menu');
+
+const menuCardsMarkup = menuElemsTmpl(menuElems);
+
+menuContainer.insertAdjacentHTML('beforeend', menuCardsMarkup);
+
+//==================== PAGE THEME CHANGING ====================
+const Theme = { LIGHT: 'light-theme', DARK: 'dark-theme' };
+const themeSwitchToggle = document.querySelector('#theme-switch-toggle');
+const body = document.querySelector('body');
+
+themeSwitchToggle.addEventListener('change', onSwitchTheme);
+
+function onSwitchTheme(event) {
+  if (event.target.checked) {
+    body.classList.add(Theme.DARK);
+    body.classList.remove(Theme.LIGHT);
+    localStorage.setItem('theme', Theme.DARK);
+  } else {
+    body.classList.add(Theme.LIGHT);
+    body.classList.remove(Theme.DARK);
+    localStorage.setItem('theme', Theme.LIGHT);
+  }
+}
+
+const theme = localStorage.getItem('theme');
+
+if (theme === Theme.DARK) {
+  themeSwitchToggle.checked = true;
+  body.classList.add(Theme.DARK);
+} else {
+  body.classList.remove(Theme.DARK);
+  body.classList.add(Theme.LIGHT);
+}
